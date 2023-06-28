1e8af4862cbc0fe2757b13d3da6d9356a67a7b23
nithiishkumarb <nithiishkumarb@gmail.com>
2023-06-28 18:08:12 +0530

    slider over

---
+++ script.js
@@ -0,0 +1,99 @@
+const wrapper=document.querySelector(".sliderwrapper");
+const menuitems=document.querySelectorAll(".menuitem");
+const products = [
+    {
+      id: 1,
+      title: "Air Force",
+      price: 119,
+      colors: [
+        {
+          code: "black",
+          img: "./img/air.png",
+        },
+        {
+          code: "darkblue",
+          img: "./img/air2.png",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Air Jordan",
+      price: 149,
+      colors: [
+        {
+          code: "lightgray",
+          img: "./img/jordan.png",
+        },
+        {
+          code: "green",
+          img: "./img/jordan2.png",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Blazer",
+      price: 109,
+      colors: [
+        {
+          code: "lightgray",
+          img: "./img/blazer.png",
+        },
+        {
+          code: "green",
+          img: "./img/blazer2.png",
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "Crater",
+      price: 129,
+      colors: [
+        {
+          code: "black",
+          img: "./img/crater.png",
+        },
+        {
+          code: "lightgray",
+          img: "./img/crater2.png",
+        },
+      ],
+    },
+    {
+      id: 5,
+      title: "Hippie",
+      price: 99,
+      colors: [
+        {
+          code: "gray",
+          img: "./img/hippie.png",
+        },
+        {
+          code: "black",
+          img: "./img/hippie2.png",
+        },
+      ],
+    },
+  ];
+let choosenproduct=products[0];
+
+const currentimg=document.querySelector(".productimg")
+const currenttitle=document.querySelector(".productstitle")
+const currentprice=document.querySelector(".productprice")
+const currentcolor=document.querySelectorAll(".color")
+const currentsize=document.querySelectorAll(".size")
+
+menuitems.forEach((item,index)=>{
+    item.addEventListener("click",()=>{
+        wrapper.style.transform=`translate(${-100*index}vw)`
+        choosenproduct=products[index]
+        currenttitle.textContent=choosenproduct.title;
+        currentprice.textContent="$"+choosenproduct.price;
+        currentimg.src=choosenproduct.colors[0].img;
+        currentcolor.forEach((color,index)=>{
+            color.style.backgroundColor=choosenproduct.colors[index].code;
+        })
+    })
+})
